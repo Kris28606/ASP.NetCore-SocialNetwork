7708b69788eb045775c39988144790eb9ab7703f
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-26 21:24:24 +0200

    Kreirani Dto objekti za Log in i Registraciju

---
+++ SocialNetwork/Dto/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SocialNetwork.Dto
+{
+    public class LoginDto
+    {
+        public String Username { get; set; }
+        public String Password { get; set; }
+    }
+}
---
+++ SocialNetwork/Dto/RegisterDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SocialNetwork.Dto
+{
+    public class RegisterDto
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+        public String Username { get; set; }
+        public String Password { get; set; }
+
+    }
+}
