440029911c6ba725dbc24711b79ae5cc388e0e72
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-26 21:23:18 +0200

    Kreiran UserRepository interfejs i njegova implementacija

---
+++ DataAccessLayer/Implementation/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLayer.Interfaces;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Implementation
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserContext context;
+
+        public UserRepository(UserContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(User entity)
+        {
+            context.Users.Add(entity);
+        }
+
+        public void Delete(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAll()
+        {
+            return context.Users.ToList();
+        }
+
+        public User SearchById(User entity)
+        {
+            return context.Users.Single(s => s.Id == entity.Id);
+        }
+
+        public void Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccessLayer/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        public void Add(TEntity entity);
+        public void Update(TEntity entity);
+        public List<TEntity> GetAll();
+        public void Delete(TEntity entity);
+        public TEntity SearchById(TEntity entity);
+    }
+}
