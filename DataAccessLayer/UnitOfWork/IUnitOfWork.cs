7f3f9d5389aa41a392319e4ff6ca5445f43008bd
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-26 21:23:50 +0200

    Kreiran UnitOfWork interfejs i njegova implementacija

---
+++ DataAccessLayer/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        public IUserRepository UserRepository { get; set; }
+        public void Save();
+    }
+}
---
+++ DataAccessLayer/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer.Implementation;
+using DataAccessLayer.Interfaces;
+using Domain;
+
+namespace DataAccessLayer.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly UserContext context;
+
+        public UnitOfWork(UserContext context)
+        {
+            this.UserRepository = new UserRepository(context);
+            this.context = context;
+        }
+
+        public IUserRepository UserRepository { get; set; }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+    }
+}
