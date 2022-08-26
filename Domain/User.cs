da90bf732de29fe81c0cfc79b09b3a6c9b8c6453
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-26 12:27:10 +0200

    Add project files.

---
+++ Domain/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public String Username { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+    }
+}
