d4f531a4992b742bdae68c28a36150f26fdd009a
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-26 21:25:42 +0200

    Implementirana ruta za Log in i Registraciju

---
+++ SocialNetwork/Controllers/UserAuthentificationController.cs
@@ -0,0 +1,55 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Dto;
+using WebApi2.Auth;
+
+namespace SocialNetwork.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class UserAuthentificationController : ControllerBase
+    {
+        private readonly JwtAuthentification jwt;
+        private readonly UserManager<User> manager;
+
+        public UserAuthentificationController(JwtAuthentification jwt, UserManager<User> manager)
+        {
+            this.jwt = jwt;
+            this.manager = manager;
+        }
+
+
+        [HttpPost]
+        [Route("/login")]
+        public async Task<IActionResult> LogIn([FromBody]LoginDto dto)
+        {
+            var token = await jwt.AuthentificationAsync(dto.Username, dto.Password);
+            if (token == null) return Unauthorized();
+            return Ok(token);
+        }
+
+        [HttpPost]
+        [Route("/register")]
+        public async Task<IActionResult> Register([FromBody]RegisterDto dto)
+        {
+            var newUser = new User
+            {
+                FirstName = dto.FirstName,
+                LastName = dto.LastName,
+                Email = dto.Email,
+                UserName = dto.Username
+            };
+
+            var result = await manager.CreateAsync(newUser, dto.Password);
+            if (result.Succeeded)
+            {   
+                return Ok("Uspesna registracija");
+            } else
+            {
+                return BadRequest("Neuspesno!"+result.Errors);
+            }
+        }
+    }
+}
--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -1,10 +1,35 @@
+using Domain;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using WebApi2.Auth;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<JwtAuthentification>();
+builder.Services.AddDbContext<UserContext>();
+builder.Services.AddIdentity<User, IdentityRole<int>>().AddEntityFrameworkStores<UserContext>();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAuthentication(options=> {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            ValidateIssuerSigningKey = true,
+            // def kako da se enkoduje
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("NapredneNetTehnologijeTokenZaAuthentifikaciju"))
+
+        };
+    });
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -17,6 +42,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
