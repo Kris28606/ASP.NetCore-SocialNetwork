a6163acb4ef34f7b445048e8fabd4cb0a2ad6b96
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-30 13:34:23 +0200

    Napravljen kontroler za slike

---
+++ SocialNetwork/Controllers/PictureController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.PictureUpload;
+
+namespace SocialNetwork.Controllers
+{
+    [Route("picture/")]
+    [ApiController]
+    public class PictureController : ControllerBase
+    {
+        private readonly IWebHostEnvironment env;
+
+        public PictureController(IWebHostEnvironment env)
+        {
+            this.env = env;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("upload")]
+        public IActionResult UploadPicture()
+        {
+            var file = HttpContext.Request.Form.Files[0];
+            try
+            {
+                Uploader u = new Uploader(env);
+                return Ok(u.Upload2((FormFile)file));
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
