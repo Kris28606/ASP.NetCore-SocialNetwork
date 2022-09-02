d1a6db44ea5912edd82ddbe5c53963dbb50a1b5e
Kris28606 <stanisavljevickristina28@gmail.com>
2022-09-03 01:42:12 +0200

    Napravljen Message maper

---
+++ Mapper/MessageMapper.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapper
+{
+    public class MessageMapper : GenericMapper<MessageDto, Message>
+    {
+        public MessageDto toDto(Message entity)
+        {
+            MessageDto dto = new MessageDto();
+            dto.MessageId=entity.MessageId;
+            dto.FromId = entity.FromId;
+            dto.ForId = entity.ForId;
+            dto.MessageText = entity.MessageText;
+            dto.Time = entity.Time;
+            return dto;
+        }
+
+        public Message toEntity(MessageDto dto)
+        {
+            Message m = new Message();
+            m.ForId = dto.ForId;
+            m.FromId = dto.FromId;
+            m.MessageText = dto.MessageText;
+            return m;
+        }
+    }
+}
