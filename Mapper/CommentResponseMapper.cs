cad71187438d0a03a8c4a2436cab41145b926576
Kris28606 <stanisavljevickristina28@gmail.com>
2022-09-06 00:29:58 +0200

    Napravljen maper za CommentResponse

---
+++ Mapper/CommentResponseMapper.cs
@@ -0,0 +1,28 @@
+﻿using Domain;
+using Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapper
+{
+    public class CommentResponseMapper : GenericMapper<CommentResponse, Comment>
+    {
+        UserMapper userMapper = new UserMapper();
+        public CommentResponse toDto(Comment entity)
+        {
+            CommentResponse res = new CommentResponse();
+            res.CommentText = entity.CommentText;
+            res.User = userMapper.toDto(entity.User);
+            res.PostId = entity.PostId;
+            return res;
+        }
+
+        public Comment toEntity(CommentResponse dto)
+        {
+            return null;
+        }
+    }
+}
