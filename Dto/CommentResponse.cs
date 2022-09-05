4aebc76777cdd604a92c3462da6171df08f999e3
Kris28606 <stanisavljevickristina28@gmail.com>
2022-09-06 00:29:39 +0200

    Napravljen Request i Response za Comment

---
+++ Dto/CommentResponse.cs
@@ -0,0 +1,16 @@
+﻿using SocialNetwork.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dto
+{
+    public class CommentResponse : MyDto
+    {
+        public UserDto User { get; set; }
+        public int PostId { get; set; }
+        public String CommentText { get; set; }
+    }
+}
