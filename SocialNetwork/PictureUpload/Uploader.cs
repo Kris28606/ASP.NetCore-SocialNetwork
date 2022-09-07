28a763dd1a8ddbcae75a0b3539eeb3c33b95bf67
Kris28606 <stanisavljevickristina28@gmail.com>
2022-09-07 22:01:40 +0200

    Izbrisana nepotrebna metoda

--- SocialNetwork/PictureUpload/Uploader.cs
+++ SocialNetwork/PictureUpload/Uploader.cs
@@ -10,26 +10,6 @@ namespace SocialNetwork.PictureUpload
         {
             this.env = env;
         }
-        public String SaveAndCreatePath(FormFile file, string baseUrl)
-        {
-            try
-            {
-                FileInfo fi = new FileInfo(file.FileName);
-                var newFileName = "Image_" + DateTime.Now.Millisecond + fi.Extension;
-                var path = Path.Combine(env.WebRootPath, "uploads", newFileName);
-
-                using (var fileStream = new FileStream(path, FileMode.Create))
-                {
-                    file.CopyTo(fileStream);
-                }
-
-                return baseUrl + "/uploads/" + newFileName;
-            }
-            catch (Exception)
-            {
-                throw new Exception("Ne mogu da sacuvam sliku!");
-            }
-        }
 
         public string Upload2(FormFile file)
         {
