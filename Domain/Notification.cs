f3d1c8b39f36916cfa817f33905a89cb726d1fd3
Kris28606 <stanisavljevickristina28@gmail.com>
2022-09-08 14:14:27 +0200

    Dodato nasledjivanje MyEntity klase

--- Domain/Notification.cs
+++ Domain/Notification.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    public class Notification
+    public class Notification : MyEntity
     {
         public int FromWhoId { get; set; }
         public User FromWho { get; set; }
