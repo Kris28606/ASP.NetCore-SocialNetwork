e5a827c99af9e0bdd6cd9ae51af86038d1088a2e
Kris28606 <stanisavljevickristina28@gmail.com>
2022-09-08 14:14:45 +0200

    Napravljen Dto za LikeNotification

---
+++ Dto/LikeNotificationDto.cs
@@ -0,0 +1,16 @@
+﻿using SocialNetwork.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dto
+{
+    public class LikeNotificationDto : MyDto
+    {
+        public UserDto FromWho { get; set; }
+        public PostResponse Post { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
