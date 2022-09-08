4170fbcfdffbe2e07857d661e08d74dc709ae250
Kris28606 <stanisavljevickristina28@gmail.com>
2022-09-08 14:14:59 +0200

    Napravljen maper za LikeNotification

---
+++ Mapper/LikeNotificationMapper.cs
@@ -0,0 +1,34 @@
+﻿using Domain;
+using Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapper
+{
+    public class LikeNotificationMapper : GenericMapper<LikeNotificationDto, LikeNotification>
+    {
+        private UserMapper userMapper;
+        private PostResponseMapper postMapper;
+        public LikeNotificationMapper()
+        {
+            this.userMapper = new UserMapper();
+            this.postMapper = new PostResponseMapper();
+        }
+        public LikeNotificationDto toDto(LikeNotification entity)
+        {
+            LikeNotificationDto dto = new LikeNotificationDto();
+            dto.Date = entity.Date;
+            dto.Post = postMapper.toDto(entity.Post);
+            dto.FromWho = userMapper.toDto(entity.FromWho);
+            return dto;
+        }
+
+        public LikeNotification toEntity(LikeNotificationDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
