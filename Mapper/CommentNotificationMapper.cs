26c54df33fd3388ff2424219ad8a51b275eae0c8
Kris28606 <stanisavljevickristina28@gmail.com>
2022-09-08 14:28:39 +0200

    Napravljen maper za CommentNotfication

---
+++ Mapper/CommentNotificationMapper.cs
@@ -0,0 +1,37 @@
+﻿using Domain;
+using Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapper
+{
+    public class CommentNotificationMapper : GenericMapper<CommentNotificationDto, CommentNotification>
+    {
+        private UserMapper userMapper;
+        private PostResponseMapper postMapper;
+        public CommentNotificationMapper()
+        {
+            this.userMapper = new UserMapper();
+            this.postMapper = new PostResponseMapper();
+        }
+
+        public CommentNotificationDto toDto(CommentNotification entity)
+        {
+            CommentNotificationDto dto = new CommentNotificationDto();
+            dto.Date = entity.Date;
+            dto.FromWho = userMapper.toDto(entity.FromWho);
+            dto.Post = postMapper.toDto(entity.Post);
+            dto.Comment = entity.Comment;
+
+            return dto;
+        }
+
+        public CommentNotification toEntity(CommentNotificationDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
