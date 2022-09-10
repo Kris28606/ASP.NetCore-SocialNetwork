695e550307049b6f77e19b5529e9bacf66815b0a
Kris28606 <stanisavljevickristina28@gmail.com>
2022-09-10 11:56:29 +0200

    Napravljen FollowNotificationDto

---
+++ Dto/FollowNotificationDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dto
+{
+    public class FollowNotificationDto : MyDto
+    {
+        public int FromWhoId { get; set; }
+        public string FromWhoUsername { get; set; }
+        public string FromWhoPicture { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Mapper/FollowNotificationMapper.cs
@@ -0,0 +1,28 @@
+﻿using Domain;
+using Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapper
+{
+    public class FollowNotificationMapper : GenericMapper<FollowNotificationDto, FollowNotification>
+    {
+        public FollowNotificationDto toDto(FollowNotification entity)
+        {
+            FollowNotificationDto dto = new FollowNotificationDto();
+            dto.FromWhoPicture = entity.FromWho.ProfilePicture;
+            dto.FromWhoUsername = entity.FromWho.UserName;
+            dto.FromWhoId = entity.FromWhoId;
+            dto.Date = entity.Date;
+            return dto;
+        }
+
+        public FollowNotification toEntity(FollowNotificationDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
