2434c80f51a5ac3ecc4b338c7fae12a6a96d68d1
Kris28606 <stanisavljevickristina28@gmail.com>
2022-09-13 13:45:12 +0200

    Napravljen hub model UserInfo

---
+++ SocialNetwork/HubModel/UserInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace SocialNetwork.HubModel
+{
+    public class UserInfo
+    {
+        public int UserId { get; set; }
+        public String ConnectionId { get; set; }
+        public String Username { get; set; }
+    }
+}
